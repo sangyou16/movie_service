c17cbb7f9b52c4f70c9a8c7399fd2347c8c09275
sangyou <sangyoupark12@gmail.com>
2023-03-19 20:23:24 +0900

    movie-service

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
+import styles from "./Movie.module.css"
 
-function Movie({coverImg,title,summary,genres,id}){ // line 9.. now when it clicks the link it will add /movie/{id numb}
+function Movie({coverImg,title,summary,genres,id,year}){ // line 9.. now when it clicks the link it will add /movie/{id numb}
 
-    return <div>
-    <img src= {coverImg} alt ={title} />
-    <h2>
-      <Link to ={`/movie/${id}`}>{title}</Link>
+    return <div className = {styles.movie}>
+    <img src= {coverImg} alt ={title} className = {styles.movie__img}/>
+    <div>
+      <h2 className = {styles.movie__title}>
+        <Link to = {`/movie/${id}`}>{title}</Link>
       </h2> 
-    <p>{summary}</p>
-    <ul>
+    <h3 className = {styles.movie__year}>{year}</h3>
+    <p>{summary.length > 235 ? `${summary.slice(0,235)}...` :summary }</p>
+    <ul className = {styles.movie__genres}>
       {genres.map((g) => (<li key = {genres}>{g}</li>))} 
     </ul>
-    
+
+    </div>
     </div> ;
 }; 
 
@@ -24,3 +28,4 @@ Movie.propTypes ={ //check prop types of the Movie components
     genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 export default Movie;
+
