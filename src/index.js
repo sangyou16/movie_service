cc9c147df0b7f41b4f14f35c8b3f983d281a57b9
sangyou <sangyoupark12@gmail.com>
2023-03-19 21:37:01 +0900

    movie-service

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ReactDOM.render(
+    <React.StrictMode>
+      <Router basename={process.env.PUBLIC_URL}>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
 
 //import "./styles.css"; //create a function in styles.css and import it to here... But this applies to ALL the buttons
 
